48009503f2258b340111beb67e91e4796b9003b0
AfkZa4aem <nastypinagrably@gmail.com>
2017-02-28 21:14:17 +0300

    add test car

--- car_spec.rb
+++ car_spec.rb
@@ -3,11 +3,24 @@ require './car'
 describe Car do
 
   before do
+    # arrange
     @car = Car.new
   end
 
   it "has fuel 0 when initialized" do
     expect(@car.fuel).to eq 0
+  end
+
+  it "can add fuel" do
+    expect(@car.add_fuel(10)).to eq 10
+  end
+
+  it "can calculate range" do
+    # act
+    @car.add_fuel 10
+
+    # assert
+    expect(@car.range).to eq 200
   end
   
 end
